c3ee6f890f7ca8b95f70766472ec946779a3cf7d
Gustavo <gustavo190371@gmail.com>
2020-06-18 17:26:29 -0300

    up 2

--- darede-backend/handler.js
+++ darede-backend/handler.js
@@ -1,18 +1,150 @@
 'use strict';
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const uuid = require('uuid/v4');
 
-module.exports.hello = async event => {
+const postsTable = process.env.POSTS_TABLE;
+
+function response(statusCode, message) {
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+}
+function sortByDate(a, b) {
+  if (a.createdAt > b.createdAt) {
+    return -1;
+  } else return 1;
+}
+// criando o post
+module.exports.createPost = (event, context, callback) => {
+  const reqBody = JSON.parse(event.body);
+
+  if (
+    !reqBody.title ||
+    reqBody.title.trim() === '' ||
+    !reqBody.body ||
+    reqBody.body.trim() === ''
+  ) {
+    return callback(
       null,
-      2
-    ),
+      response(400, {
+        error: 'É preciso ter um título e um corpo,não pode estar vazio'
+      })
+    );
+  }
+
+  const post = {
+    id: uuid(),
+    createdAt: new Date().toISOString(),
+    userId: 1,
+    title: reqBody.title,
+    body: reqBody.body
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return db
+    .put({
+      TableName: postsTable,
+      Item: post
+    })
+    .promise()
+    .then(() => {
+      callback(null, response(201, post));
+    })
+    .catch((err) => response(null, response(err.statusCode, err)));
+};
+// Listar
+module.exports.getAllPosts = (event, context, callback) => {
+  return db
+    .scan({
+      TableName: postsTable
+    })
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+
+// Retornar o numero de posts
+module.exports.getPosts = (event, context, callback) => {
+  const numberOfPosts = event.pathParameters.number;
+  const params = {
+    TableName: postsTable,
+    Limit: numberOfPosts
+  };
+  return db
+    .scan(params)
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// buscar por ID 
+module.exports.getPost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: postsTable
+  };
+
+  return db
+    .get(params)
+    .promise()
+    .then((res) => {
+      if (res.Item) callback(null, response(200, res.Item));
+      else callback(null, response(404, { error: 'Nada encontrado' }));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Atualizar
+module.exports.updatePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const reqBody = JSON.parse(event.body);
+  const { body, title } = reqBody;
+
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: postsTable,
+    ConditionExpression: 'attribute_exists(id)',
+    UpdateExpression: 'SET title = :title, body = :body',
+    ExpressionAttributeValues: {
+      ':title': title,
+      ':body': body
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+  console.log('Atualizando');
+
+  return db
+    .update(params)
+    .promise()
+    .then((res) => {
+      console.log(res);
+      callback(null, response(200, res.Attributes));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Delete
+module.exports.deletePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: postsTable
+  };
+  return db
+    .delete(params)
+    .promise()
+    .then(() =>
+      callback(null, response(200, { message: 'Deletado com sucesso' }))
+    )
+    .catch((err) => callback(null, response(err.statusCode, err)));
 };
