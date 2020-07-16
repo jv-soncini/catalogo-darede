aa44aece2a5353edd3a204805c887ada2a6292bf
FIC <FIC@info.local>
2020-07-16 14:37:11 -0300

    add script to execute, but have some problems with the params to execute

---
+++ darede-backend/CloudForm/EC2Execute.js
@@ -0,0 +1,34 @@
+'use strict'
+const AWS = require('aws-sdk');
+const template = require('../Templates/Webserver-Debian-Apache-Custom.json')
+const cloudformation = new AWS.CloudFormation()
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+module.exports.execute =  (event, context, callback) => {
+    
+    const params = {
+         
+            StackName: 'EC2',
+            TemplateBody: JSON.stringify(template),
+            TimeoutInMinutes: 5
+          
+        }
+        
+        cloudformation.createStack(params, (err, data) => {
+          if (err){
+
+            callback(null,{
+              statuscode: err.statusCode || 501,
+              Erro: err,
+            })
+            return; 
+          } 
+
+
+         const response = {
+           statuscode: 201,
+           data: data
+         }
+
+          callback(null, response)          
+        });
+}
---
+++ darede-backend/Service/create.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); 
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(JSON.stringify(event.body));
+  const NomeServico = data.NomeServico
+  const DescricaoServico = data.DescricaoServico
+  if (typeof NomeServico !== 'string' || typeof DescricaoServico !== 'string' ) {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t create the todo item.'));
+    return;
+  }
+
+  const params = {
+    TableName: process.env.SERVICE_TABLE,
+    Item: {
+      id: uuid.v1(),
+      NomeServico: NomeServico,
+      DescricaoServico: DescricaoServico,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+
+  dynamoDb.put(params, (error) => {
+   
+    if (error) {
+      console.error(error);
+      callback(new Error('nao foi possivel criar o serviço'));
+      return;
+    }
+
+    
+    const response = {
+      statusCode: 200,
+      body: params.Item,
+    };
+    callback(null, response);
+  });
+};
+
+
+
+
+
+
+
