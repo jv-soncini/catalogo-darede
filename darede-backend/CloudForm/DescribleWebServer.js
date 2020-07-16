01192c44f5ce227e3f718792f8a9cc1539c77eea
FIC <FIC@info.local>
2020-07-16 16:58:09 -0300

    script executed, but actives the rollback

---
+++ darede-backend/CloudForm/DescribleWebServer.js
@@ -0,0 +1,29 @@
+'use strict'
+const AWS = require('aws-sdk');
+const cloudFormation = new AWS.CloudFormation();
+
+module.exports.describle = (event, context, callback) => {
+   
+    var params = {};
+
+      cloudFormation.describeStacks(params, (err, data) => {
+        if (err){
+
+          callback(null,{
+            statuscode: err.statusCode || 501,
+            Erro: err,
+          })
+          return; 
+        } 
+
+
+       const response = {
+         statuscode: 200,
+         data: data
+       }
+
+       callback(null, response)
+})
+
+}
+
--- darede-backend/CloudForm/EC2Execute.js
+++ darede-backend/CloudForm/EC2Execute.js
@@ -1,6 +1,6 @@
 'use strict'
 const AWS = require('aws-sdk');
-const template = require('../Templates/Webserver-Debian-Apache-Custom.json')
+const template = require('../Templates/Webserver.json')
 const cloudformation = new AWS.CloudFormation()
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.execute =  (event, context, callback) => {
@@ -8,6 +8,7 @@ module.exports.execute =  (event, context, callback) => {
     const params = {
          
             StackName: 'EC2',
+            DisableRollback: true,
             TemplateBody: JSON.stringify(template),
             TimeoutInMinutes: 5
           
